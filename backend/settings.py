813d0bec7d3870dc26787a570266e93fcbef8f35
Viola <viola.bergere@gmail.com>
2025-01-17 10:33:19 +0000

    chore: delete index.html from static folder, update allowed hosts in settings

--- backend/settings.py
+++ backend/settings.py
@@ -37,7 +37,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-surfdemon-forthe50-kxfi8ykrews.ws-eu117.gitpod.io',]
+ALLOWED_HOSTS = ['8000-surfdemon-forthe50-kxfi8ykrews.ws-eu117.gitpod.io',
+                 '127.0.0.1:8000',]
 
 
 # Application definition
