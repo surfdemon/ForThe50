b5ef90fce83596e0b723bec3960357e314285c7b
SoroushGReza <soroush_developer@hotmail.com>
2025-01-17 22:09:13 +0300

    Add path to report and import it from views

--- forthe50/urls.py
+++ forthe50/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import contact, index, team
+from .views import contact, index, team, report
 
 urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
     path('team/', team, name='team'),
+    path('report/', report, name='report'),
 ]
