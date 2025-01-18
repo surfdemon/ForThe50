5a7f46e8dfbb54c121eb2687e708d17afe274337
SoroushGReza <soroush_developer@hotmail.com>
2025-01-18 22:45:59 +0300

    Delete chatbot_views.py and related path as it no longer neede.

--- forthe50/urls.py
+++ forthe50/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import contact, index, team, statistics, ReportView
-from .chatbot_views import chatbot
 
 urlpatterns = [
     path("", index, name="index"),
@@ -8,5 +7,4 @@ urlpatterns = [
     path("meet-team/", team, name="meet_team"),
     path("statistics/", statistics, name="statistics"),
     path("report/", ReportView.as_view(), name="report"),
-    path("chatbot/", chatbot, name="chatbot"),
 ]
