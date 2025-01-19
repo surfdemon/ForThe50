85c966e187b5fad240d78dc8257be881a9612b9a
SoroushGReza <soroush_developer@hotmail.com>
2025-01-19 11:30:58 +0300

    Add New About Page and Refactor CSS for Improved C
    larity:
    
    about.html:
    - Added a new "About Us" page with structured sections for mission,
      vision, approach, and impact.
    - Included a call-to-action section with buttons for contacting
      resources and reporting crimes.
    - Designed the page layout with responsiveness in mind and used Django
      template tags for static asset handling.
    
    style.css:
    - Refactored CSS into distinct sections (General Styles, Navbar, Footer,
      Pages) for better structure and maintainability.
    - Simplified and removed duplicate styles to streamline the stylesheet.
    - Enhanced button styles with hover effects and improved consistency
      across components.
    - Added responsive design improvements for better usability on smaller
      screens.
    - Organized media query breakpoints for better scaling across various
      devices.
    
    views.py:
    - Added a new `about` view function to render the "About Us" page
      template.
    
    urls.py:
    - Added a URL pattern for the "About Us" page, linking the path
      `/about/` to the `about` view function.

--- forthe50/urls.py
+++ forthe50/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import contact, index, team, statistics, ReportView, knowledge
+from .views import about, contact, index, team, statistics, ReportView, knowledge
 
 
 urlpatterns = [
     path("", index, name="index"),
+    path("about/", about, name="about"),
     path("contact/", contact, name="contact"),
     path("meet-team/", team, name="meet_team"),
     path("statistics/", statistics, name="statistics"),
