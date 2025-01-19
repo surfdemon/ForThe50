fdd3504b30aac49677dd3ddad1a1235bbc76b375
Chrysanthus Chiagwah <chrysanthusobinna@gmail.com>
2025-01-19 17:51:24 +0000

    Fix broken CSS braces, update images, hide Yandex Translate link

--- backend/static/js/script.js
+++ backend/static/js/script.js
@@ -23,3 +23,18 @@ document.addEventListener("DOMContentLoaded", () => {
     irelandButton.classList.remove("primary-btn");
   });
 });
+
+// hide link https://translate.yandex.com/
+document.addEventListener("DOMContentLoaded", function () {
+  const observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      const yandexLink = document.querySelector('a[href="https://translate.yandex.com/"]');
+      if (yandexLink) {
+        yandexLink.style.display = "none";
+        observer.disconnect(); 
+      }
+    });
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+});
